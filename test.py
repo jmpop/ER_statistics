caf87b3260a06550eab542063a311cae61fca5af
jmpop <88295468+jmpop@users.noreply.github.com>
2024-01-23 22:57:29 +0900

    Update test.py

--- test.py
+++ test.py
@@ -2,9 +2,10 @@
 # from ER_datas.test import *
 import unittest
 
-
+print("b")
 class TestForeignTeam(unittest.TestCase):
     def test_true(self):
+        print("e")
         self.assertEqual(1, 1)
 
 if __name__ == "__main__":
